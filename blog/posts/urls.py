59dfc4366c74c1cf5fd21e611eaa3fe9bcd3e1e0
BPhilpot301 <britnphilpot43@outlook.com>
2025-09-30 13:05:45 -0500

    final

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,10 +3,14 @@ from .views import (
     PostListView,
     PostDraftListView,
     PostArchivedListView,
+    PostCreateView,
+    PostDetailView,
  ) 
 
 urlpatterns = [
         path('list/', PostListView.as_view(), name='posts'),
         path('drafts/list/', PostDraftListView.as_view(), name='draft_list'),
         path('archived/list/', PostArchivedListView.as_view(), name='archived_list'),
+        path('new/', PostCreateView.as_view(), name='new'),
+        path('<int:pk>/', PostDetailView.as_view(), name="post_detail"), 
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import (
         ListView,
-        
+        CreateView,
+        DetailView,
 )
 from .models import Post, Status
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 
 class PostListView(ListView):
     template_name = "posts/list.html"
@@ -17,19 +22,49 @@ class PostListView(ListView):
         print(context)
         return(context)
 
-class PostDraftListView(ListView):
+class PostDraftListView(LoginRequiredMixin, ListView):
     template_name = "posts/draft_list.html"
     context_object_name = "draft"
     status = Status.objects.get(name="draft")
     queryset = Post.objects.filter(status=status).order_by("created_on").reverse()
     
-   
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft_posts = context['draft'].filter(author=self.request.user)
+        context['draft'] = draft_posts
+        return context
 
 
-class PostArchivedListView(ListView):
+class PostArchivedListView(LoginRequiredMixin, ListView):
     template_name = "posts/archived_list.html"
     context_object_name = "archived"
     status = Status.objects.get(name="archived")
     queryset = Post.objects.filter(status=status).order_by("created_on").reverse()
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft_posts = context['archived'].filter(author=self.request.user)
+        context['archived'] = draft_posts
+        return context
 # Create your views here.
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
